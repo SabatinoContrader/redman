c7bec916475deb29177071bb65c89ea6441184e2
NappiGennaro <g.nappi@contrader.it>
2018-07-18 17:10:42 +0200

    UserView added

---
+++ src/main/model/Utente.java
@@ -0,0 +1,50 @@
+//package main.model;
+//
+//public class Utente {
+//
+//	int idutente;
+//	int idruolo;
+//	String username;	
+//	String password; 
+//	
+//	public Utente() {}
+//	
+//	public Utente(int idutente, int idruolo, String username, String password) {
+//		
+//		
+//	}
+//	
+//	public int getIdutente() {
+//		return idutente;
+//	}
+//
+//	public void setIdutente(int idutente) {
+//		this.idutente = idutente;
+//	}
+//
+//	public int getIdruolo() {
+//		return idruolo;
+//	}
+//
+//	public void setIdruolo(int idruolo) {
+//		this.idruolo = idruolo;
+//	}
+//
+//	public String getUsername() {
+//		return username;
+//	}
+//
+//	public void setUsername(String username) {
+//		this.username = username;
+//	}
+//
+//	public String getPassword() {
+//		return password;
+//	}
+//
+//	public void setPassword(String password) {
+//		this.password = password;
+//	}
+//
+//
+//}
--- src/main/view/UserView.java
+++ src/main/view/UserView.java
@@ -1,31 +1,49 @@
 package main.view;
 
+import main.MainDispatcher;
 import main.controller.Request;
 
-public class UserView implements View{
+import java.util.List;
+import java.util.Scanner;
 
-	@Override
+public class UserView implements View {
+
+	private int choice;
+
 	public void showResults(Request request) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
 	public void showOptions() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Benvenuto in ContraderFramework");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("--------Menu--------");
+		System.out.println("-------Utente-------");
+		System.out.println("1) Visualizza stato nodi");
+		System.out.println("2) Visualizza segnalazioni");
+		System.out.println("3) Logout");
+		this.choice = Integer.parseInt(getInput());
+
 	}
 
-	@Override
-	public String getInput() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public void submit() {
+		if (choice < 1 || choice > 3)
+			MainDispatcher.getInstance().callAction("User", "doControl", null);
+		else if (choice == 3)
+			MainDispatcher.getInstance().callAction("Login", "doControl", null);
+		else {
+			Request request = new Request();
+			request.put("choice", choice);
+			MainDispatcher.getInstance().callAction("User", "doControl", request);
+		}
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	public String getInput() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
 }
+
