a8e326a314c5f895c4d78c6c65744b3e4b5282db
CiccioDamiano <f.damiano@contrader.it>
2018-08-27 18:50:29 +0200

    modifica schermate home networkmanager e user

--- src/main/java/com/contrader/redman/controller/networkManager/TasksManagementNMController.java
+++ src/main/java/com/contrader/redman/controller/networkManager/TasksManagementNMController.java
@@ -1,5 +1,8 @@
-//package com.contrader.redman.controller;
+//package com.contrader.redman.controller.networkManager;
 //
+//import com.contrader.redman.model.Task;
+//import com.contrader.redman.service.TaskService;
+//import com.contrader.redman.service.UserService;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
@@ -8,11 +11,47 @@
 //import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.RequestMapping;
 //
-//import com.contrader.redman.model.Task;
-//import com.contrader.redman.service.TaskService;
+//import java.util.ArrayList;
+//import java.util.List;
 //
 //@Controller
 //@RequestMapping("/networkManager")
 //public class TasksManagementNMController {
-//	
+//    private TaskService taskService;
+//    private UserService userService;
+//
+//    @Autowired
+//    public TasksManagementNMController(TaskService taskService,UserService userservice) {
+//        this.taskService = taskService;
+//        this.userService = userService;
+//    }
+//
+//    @RequestMapping("/listaTasks")
+//    public String GetListaTasks(Model model){
+//        List<Task> listaTasks = new ArrayList<>();
+//
+//        for (Task task : taskService.findAll()) {
+////            String idresp=nodo.getIdresponsabile();
+//            String iduser=task.getIdutente();
+//            Task newTask= (Task) task.clone();
+//
+////            if(idresp != null) {
+////                //Integer idresp = Integer.parseInt(nodostring);
+////                String resp = userService.findByIdutente(Integer.parseInt(idresp));
+////                newNodo.setIdresponsabile(resp);
+////            }
+//
+//            if(iduser != null){
+//                String user = userService.findByIdutente(Integer.parseInt(iduser));
+//                newTask.setIdutente(user);
+//            }
+//
+//            listaTasks.add(newTask);
+//        }
+//        model.addAttribute("tasks", listaTasks);
+//        model.addAttribute("nodo", new Task());
+//        model.addAttribute("mode", "listaTasks");
+//        return "/networkManager/MenùAdmin";
+//    }
+//    
 //}
--- src/main/java/com/contrader/redman/service/TaskService.java
+++ src/main/java/com/contrader/redman/service/TaskService.java
@@ -1,19 +1,22 @@
-//package com.contrader.redman.service;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import com.contrader.redman.dao.TaskDao;
-//import com.contrader.redman.model.Task;
-//
-//@Service
-//public class TaskService {
-//
-//	private TaskDao taskDao;
-//
-//	@Autowired
-//	public TaskService(TaskDao taskDao) {
-//		this.taskDao = taskDao;
-//	}
-//
-//}
+package com.contrader.redman.service;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.contrader.redman.dao.TaskRepository;
+import com.contrader.redman.model.Task;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+	private TaskRepository taskRepository;
+
+	@Autowired
+	public TaskService(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+	public List<Task> findAll() {
+       return (List<Task>) taskRepository.findAll();
+    }
+}
